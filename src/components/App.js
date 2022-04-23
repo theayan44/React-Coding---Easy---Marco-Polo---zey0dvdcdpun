9bf238bca2e9056c98da55b4d6622a85d4e3d1da
theayan44 <88241719+theayan44@users.noreply.github.com>
2022-04-23 12:47:55 +0530

    save

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
-import React from 'react'
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
 const App = () => {
-  
+  const [text, setText] = useState("Marco");
+  const [buttonText, setButtonText] = useState("Polo");
+  const handleClick = () => {
+    text === "Marco" ? setText("Polo") : setText("Marco");
+    buttonText === "Marco" ? setButtonText("Polo") : setButtonText("Marco");
+  };
   return (
     <div id="main">
-
+      <h1 id="marco-polo">{text}</h1>
+      <button id="marco-polo-toggler" onClick={handleClick}>
+        {buttonText}
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
